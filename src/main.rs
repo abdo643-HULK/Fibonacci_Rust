9a4427002f2d5e593816db90f8aca625946a54f3
abdo643 <abou.shehata643@yahoo.de>
2020-10-07 21:29:27 +0200

    finished project with recursion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    use std::io::{self,Write};
+    println!("Fibonacci generator");
+    println!("Type \"q\" to end the program");
+    println!();
+    loop {
+        let mut value = String::new();
+
+        print!("Enter a positive integer: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut value).expect("Failed to read line");
+
+        if value.trim() == "q" {
+            break;
+        }
+
+        let value: u32 = match value.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("{}", fibonacci(value));
+    }
 }
+
+fn fibonacci(num: u32) -> u32 {
+    match num {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(num - 1) + fibonacci(num - 2)
+    }
+}
+
+// fn fibonacci(num: u32) -> u32 {
+//     if(num <= 1) { return 1 };
+//     return fibonacci(num - 1) + fibonacci(num - 2);
+// }
